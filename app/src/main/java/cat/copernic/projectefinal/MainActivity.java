825c392cf2a7585821f7019ec9a9d6370408815e
gaulone <franciscogutierrez19@gmail.com>
2015-05-20 11:39:22 +0200

    2 commit

--- app/src/main/java/cat/copernic/projectefinal/MainActivity.java
+++ app/src/main/java/cat/copernic/projectefinal/MainActivity.java
@@ -4,14 +4,39 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public Usuario user;
+    EditText usuario;
+    EditText contraseña;
+    Button acceder;
+    TextView errores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usuario = (EditText) findViewById(R.id.EDUsuario);
+        contraseña = (EditText) findViewById(R.id.EDContraseña);
+        acceder = (Button) findViewById(R.id.BotonAcceder);
+        errores = (TextView) findViewById(R.id.textViewError);
+
+
     }
 
 
@@ -36,4 +61,37 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    //Leer ficheros
+    public void leerFichero(String archivo) throws FileNotFoundException, IOException {
+        String cadena;
+/*
+         File dir = new File(getFilesDir() +"/app/src/main/java/cat/copernic");
+        dir.mkdirs(); //create folders where write files
+         File file = new File(dir, "Config.txt");*/
+
+        BufferedReader fin =
+                new BufferedReader(
+                        new InputStreamReader(
+                                openFileInput(archivo)));
+
+        String texto = fin.readLine();
+        while((cadena = fin.readLine())!=null) {
+           // System.out.println(cadena);
+          //  contraseña.setText(cadena);
+        }
+        fin.close();
+    }
+
+
+    public void OnClickAcceder (View view) {
+
+
+         user=new Usuario(usuario.getText().toString(),contraseña.getText().toString());
+
+        errores.setText("ok: "+user);
+
+    }
+
 }
